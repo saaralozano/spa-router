6ae792fb3591fb1c08eb446109fa31488b0b03a5
saaralozano <saloz2001@gmail.com>
2025-03-26 20:19:38 +0100

    Páginas del Landing Page

--- src/modulorum/landing/paginae/DomusPagina.vue
+++ src/modulorum/landing/paginae/DomusPagina.vue
@@ -1,5 +1,13 @@
 <template>
 
-    <h1>Home Page</h1>
-    
+    <div class="text-center">
+        <h1 class="text-4xl sm:text-5xl font-bold tracking-tighter">
+            Bienvenido a nuestro sitio web
+        </h1>
+
+        <p class="md:text-xl text-gray-500 mx-auto w-[600px]">
+            Donum mox recipies pro fidelitate erga fructus nostros
+        </p>
+    </div>
+
 </template>
