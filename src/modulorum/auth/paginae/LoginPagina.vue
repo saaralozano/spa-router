24fc29b37ab911b9e645d6055188d5e85fdb00cc
saaralozano <saloz2001@gmail.com>
2025-03-28 17:07:25 +0100

    Login y Register

--- src/modulorum/auth/paginae/LoginPagina.vue
+++ src/modulorum/auth/paginae/LoginPagina.vue
@@ -1,30 +1,19 @@
 
 <template>
 
-  <div  class="bg-gray-100 flex justify-center items-center h-screen">
-    <div class="w-1/2 h-screen hidden lg:block">
-      <img
-        src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat"
-        alt="Placeholder Image"
-        class="object-cover w-full h-full"
-      />
-    </div>
-
-    <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-
-      <h1 class="text-2xl font-semibold mb-4">Login</h1>
+  <h1 class="text-2xl font-semibold mb-4">Login</h1>
 
       <form action="#" method="POST">
-        <div class="mb-4">
-          <label for="username" class="block text-gray-600">Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-            autocomplete="off"
-          />
-        </div>
+          <div class="mb-4">
+              <label for="username" class="block text-gray-600">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                autocomplete="off"
+             />
+          </div>
 
         <div class="mb-4">
           <label for="password" class="block text-gray-600">Password</label>
@@ -55,10 +44,7 @@
       </form>
 
       <div class="mt-6 text-blue-500 text-center">
-        <a href="#" class="hover:underline">Sign up Here</a>
-      </div>
-    </div>
-
-  </div>
+        <RouterLink :to="{name: 'register'}" class="hover:underline">Sign up Here</RouterLink>
+      </div> 
 
 </template>
